c9f14fee0ccd6e08eecea2a4db2b98bddaebcfa3
aaron.hayman4 <aaron.hayman4@600900B211DE407>
2018-02-22 11:52:50 +1100

    Created Gone Home game

---
+++ Assets/~GoneHome/Scripts/Death.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace GoneHome
+{
+    public class Death : MonoBehaviour
+    {
+
+        // Use this for initialization
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+
+        void Died()
+        {
+            // Play animation
+            // Spawn particles
+            // Perform other events
+        }
+
+        // Detect collision with other triggers
+        void OnTriggerEnter(Collider other)
+        {
+            // Have we hit a death zone or an enemy
+            if (other.name == "DeathZone" || other.name == "Enemy")
+            {
+                // Kill object
+                Died();
+            }
+        }
+
+    }
+}
---
+++ Assets/~GoneHome/Scripts/Player.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GoneHome
+{
+    public class Player : MonoBehaviour
+    {
+
+        public float movementSpeed = 20f;
+        private Rigidbody rigid;
+
+        private Transform cam;
+
+        // Use this for initialization
+        void Start()
+        {
+            rigid = GetComponent<Rigidbody>();
+
+            cam = Camera.main.transform;
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            Movement();
+        }
+
+        void Movement()
+        {
+            float inputH = Input.GetAxis("Horizontal");
+            float inputV = Input.GetAxis("Vertical");
+
+            Vector3 inputDir = new Vector3(inputH, 0, inputV);
+
+            // Whereever camera is looking, move on that axis
+            inputDir = Quaternion.AngleAxis(cam.eulerAngles.y, Vector3.up) * inputDir;
+
+
+            // Moves the position of the rigidbody without 
+            // effecting gravity
+            Vector3 position = transform.position;
+            position += inputDir * movementSpeed * Time.deltaTime;
+            rigid.MovePosition(position);
+
+        }
+
+
+
+
+    }
+
+}
