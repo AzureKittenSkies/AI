e28983eabc504e4f5c02793c6d7fef54ed063ffd
AzureSkies <azzhayman@gmail.com>
2018-06-07 11:57:28 +1000

    More inheritence, cannon and tower scripts

---
+++ Unity/Assets/~TowerDefense/Scripts/Towers/Cannon.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TowerDefense
+{
+    public class Cannon : Tower
+    {
+        #region Variables
+
+        public Transform barrel;
+        public float lineDelay = 2f;
+
+        private LineRenderer line;
+
+
+
+
+        #endregion
+
+        void Awake()
+        {
+            line = GetComponent<LineRenderer>();
+        }
+
+        public override void Aim(Enemy e)
+        {
+            // line from center point
+            line.SetPosition(0, transform.position);
+            // line to barrel
+            line.SetPosition(1, barrel.position);
+            // line to enemy
+            line.SetPosition(2, e.transform.position);
+
+        }
+
+        public override void Attack(Enemy e)
+        {
+            
+        }
+
+    }
+}
---
+++ Unity/Assets/~TowerDefense/Scripts/Towers/Tower.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TowerDefense
+{
+    // tower is a monobehaivour
+    public class Tower : MonoBehaviour
+    {
+        #region Variables
+        public float damage = 10f;
+        public float attackDelay = 1f;
+
+        // tower has an enemy
+        protected Enemy currentEnemy;
+
+        private float attackTimer = 0;
+
+
+
+
+        #endregion
+
+        public virtual void Aim(Enemy e) { }
+        public virtual void Attack(Enemy e) { }
+
+        protected virtual void Update()
+        {
+            attackTimer += Time.deltaTime;
+            // if there is a currentEnemy the tower is targeting
+            if (currentEnemy)
+            {
+                // aim at current enemy
+                Aim(currentEnemy);
+                if (attackTimer >attackDelay)
+                {
+                    // attack the current enemy
+                    Attack(currentEnemy);
+                    attackTimer = 0f;
+                }
+            }
+        }
+
+        void OnTriggerEnter(Collider other)
+        {
+            Enemy enemy = other.GetComponent<Enemy>();
+            if (enemy!= null && currentEnemy == null)
+            {
+                currentEnemy = enemy;
+            }
+        }
+
+        void OnTriggerExit(Collider other)
+        {
+            Enemy enemy = other.GetComponent<Enemy>();
+            if (enemy != null && currentEnemy == enemy)
+            {
+
+            }
+        }
+    }
+}
